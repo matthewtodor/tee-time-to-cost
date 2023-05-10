e96f5a7bf1bab849af6229477953f4dcec058fa0
Matthew Todor <unclejunglegeorge@gmail.com>
2023-05-09 21:14:19 -0600

    starting with the math for total number of minutes of play

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
 import "./App.css";
 
+/**
+@param {number} cost - The cost per individual for a tee time
+@param {string} firstTime - the first tee time available in a day
+@param {string} lastTime - the last tee time available in a day
+@param {string} teeTimeDuration - the time between tee times
+*/
+const teeTimeCost = (cost, firstTime, lastTime, teeTimeDuration) => {
+	const sanitizedFirstTime = () => {
+		if (firstTime.includes("AM")) {
+			const timeArr = firstTime.split(" ").shift().split(":");
+			let hr = parseInt(timeArr[0]);
+			if (hr === 12) {
+				hr = hr - 12;
+			}
+			const mn = parseInt(timeArr[1]);
+			const hrToMn = hr * 60 + mn;
+			return hrToMn;
+		}
+		const timeArr = firstTime.split(" ").shift().split(":");
+		const hr = parseInt(timeArr[0]) + 12;
+		const mn = parseInt(timeArr[1]);
+		const hrToMn = hr * 60 + mn;
+		return hrToMn;
+	};
+	const sanitizedLastTime = () => {
+		if (lastTime.includes("AM")) {
+			const timeArr = lastTime.split(" ").shift().split(":");
+			let hr = parseInt(timeArr[0]);
+			if (hr === 12) {
+				hr = hr - 12;
+			}
+			const mn = parseInt(timeArr[1]);
+			const hrToMn = hr * 60 + mn;
+			return hrToMn;
+		}
+		const timeArr = lastTime.split(" ").shift().split(":");
+		const hr = parseInt(timeArr[0]) + 12;
+		const mn = parseInt(timeArr[1]);
+		const hrToMn = hr * 60 + mn;
+		return hrToMn;
+	};
+	const totalMinutesofPlay = sanitizedLastTime() - sanitizedFirstTime();
+	console.log(Math.floor(totalMinutesofPlay / parseInt(teeTimeDuration)));
+};
+
 function App() {
+	teeTimeCost(1, "12:30 AM", "8:00 PM", "12");
 	return (
 		<div className="App">
 			<header className="App-header">
